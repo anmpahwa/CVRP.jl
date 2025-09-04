6a438ab0328251170985a65fb90a99d8f078ad7f
anmpahwa <anmpahwa@civil.iitm.ac.in>
2025-09-04 22:38:06 +0530

    update operations.jl

--- src/operations.jl
+++ src/operations.jl
@@ -1,18 +1,18 @@
-function removenode!(s::Solution, n::Node, p::Node, q::Node, v::Vehicle)
+function removenode!(s::Solution, n::Node, t::Node, h::Node, v::Vehicle)
     # fetch features
     x = v.x * v.n
     y = v.y * v.n
-    aₜᵢ = s.A[p.i, n.i]
-    aᵢₕ = s.A[n.i, q.i]
-    aₜₕ = s.A[p.i, q.i]
-    δ = (aₜᵢ.l + aᵢₕ.l) - aₜₕ.l
+    aₜₙ = s.A[t.i, n.i]
+    aₙₕ = s.A[n.i, h.i]
+    aₜₕ = s.A[t.i, h.i]
+    δ = (aₜₙ.l + aₙₕ.l) - aₜₕ.l
     # update node
     n.t = 0
     n.h = 0
     n.v = 0
     # update predecessor and successor nodes
-    p.h = q.i
-    q.t = p.i
+    t.h = h.i
+    h.t = t.i
     # update associated vehicle
     v.n -= -1
     v.l -= n.q
@@ -24,21 +24,21 @@ function removenode!(s::Solution, n::Node, p::Node, q::Node, v::Vehicle)
     return s
 end
 
-function insertnode!(s::Solution, n::Node, p::Node, q::Node, v::Vehicle)
+function insertnode!(s::Solution, n::Node, t::Node, h::Node, v::Vehicle)
     # fetch features
     x = v.x * v.n
     y = v.y * v.n
-    aₜᵢ = s.A[p.i, n.i]
-    aᵢₕ = s.A[n.i, q.i]
-    aₜₕ = s.A[p.i, q.i]
-    δ = (aₜᵢ.l + aᵢₕ.l) - aₜₕ.l
+    aₜₙ = s.A[t.i, n.i]
+    aₙₕ = s.A[n.i, h.i]
+    aₜₕ = s.A[t.i, h.i]
+    δ = (aₜₙ.l + aₙₕ.l) - aₜₕ.l
     # update node
-    n.t = p.i
-    n.h = q.i
+    n.t = t.i
+    n.h = h.i
     n.v = v.i
     # update predecessor and successor nodes
-    p.h = n.i
-    q.t = n.i
+    t.h = n.i
+    h.t = n.i
     # update associated vehicle
     v.n += 1
     v.l += n.q
