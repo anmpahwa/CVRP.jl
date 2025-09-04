c5be320a5be8e1b0a7c498ecd2d191770e4057db
anmpahwa <anmpahwa@civil.iitm.ac.in>
2025-09-04 22:19:32 +0530

    update CVRP.jl

--- src/CVRP.jl
+++ src/CVRP.jl
@@ -10,10 +10,16 @@ include("datastructure.jl")
 include("functions.jl")
 include("initialize.jl")
 include("operations.jl")
+include("remove.jl")
+include("insert.jl")
+include("localsearch.jl")
 include("parameters.jl")
 include("ALNS.jl")
 include("visualize.jl")
 
-export build, initialize, vectorize, visualize
+export  build, 
+        vectorize, f, 
+        ALNSparameters, ALNS, 
+        visualize
 
 end
