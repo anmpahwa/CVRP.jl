9bb4600fc76f08f1bf73a3c02f94a1ebcf42a7a2
anmpahwa <anmpahwa@civil.iitm.ac.in>
2025-09-07 13:36:01 +0530

    add comments

--- src/remove.jl
+++ src/remove.jl
@@ -1,6 +1,9 @@
 function randomcustomer!(rng::AbstractRNG, k::Int, s::Solution)
+    # set node weights: uniform
     W = [isone(i) ? 0 : 1 for i ∈ eachindex(s.N)]
+    # sample node indices
     I = sample(rng, k, eachindex(s.N), Weights(W))
+    # loop: remove exactly k sampled nodes
     for i ∈ I
         n = s.N[i]
         t = s.N[n.t]
@@ -12,9 +15,13 @@ function randomcustomer!(rng::AbstractRNG, k::Int, s::Solution)
 end
 
 function relatedcustomer!(rng::AbstractRNG, k::Int, s::Solution)
+    # randomize a pivot node
     j = rand(rng, 2:lastindex(s.N))
+    # set node weights: relatedness
     W = [isone(i) ? 0. : relatedness(s.N[i], s.N[j]) for i ∈ eachindex(s.N)]
+    # sample node indices
     I = sample(rng, k, eachindex(s.N), Weights(W))
+    # loop: remove exactly k sampled nodes
     for i ∈ I
         n = s.N[i]
         t = s.N[n.t]
@@ -26,6 +33,7 @@ function relatedcustomer!(rng::AbstractRNG, k::Int, s::Solution)
 end
 
 function worstcustomer!(rng::AbstractRNG, k::Int, s::Solution)
+    # set node weights: cost
     W = zeros(Float64, length(s.N))
     z = f(s)
     for i ∈ eachindex(s.N)
@@ -39,7 +47,9 @@ function worstcustomer!(rng::AbstractRNG, k::Int, s::Solution)
         W[i] = z - z′
         insertnode!(n, p, q, v, s)
     end
+    # sample node indices
     I = sample(rng, k, eachindex(s.N), Weights(W))
+    # loop: remove exactly k sampled nodes
     for i ∈ I
         n = s.N[i]
         t = s.N[n.t]
@@ -51,12 +61,16 @@ function worstcustomer!(rng::AbstractRNG, k::Int, s::Solution)
 end
 
 function randomvehicle!(rng::AbstractRNG, k::Int, s::Solution)
+    # set vehicle weights: uniform
     W = ones(Float64, length(s.V))
+    # loop: until at least k nodes are removed
     m = 0
     while true
+        # sample a vehicle
         j = rand(rng, eachindex(s.V), Weights(W))
         v = s.V[j]
         i = v.s
+        # remove all associated nodes
         for _ ∈ 1:v.n
             n = s.N[i]
             t = s.N[n.t]
@@ -65,6 +79,7 @@ function randomvehicle!(rng::AbstractRNG, k::Int, s::Solution)
             removenode!(n, t, h, v, s)
             m += 1
         end
+        # update vehicle weight
         W[j] = 0.
         if m ≥ k break end
     end
@@ -73,13 +88,18 @@ end
 
 
 function relatedvehicle!(rng::AbstractRNG, k::Int, s::Solution)
+    # randomize a pivot vehicle
     j = rand(rng, eachindex(s.V))
+    # set vehicle weights: relatedness
     W = [relatedness(s.V[i], s.V[j]) for i ∈ eachindex(s.V)]
+    # loop: until at least k nodes are removed
     m = 0
     while true
+        # sample a vehicle
         j = rand(rng, eachindex(s.V), Weights(W))
         v = s.V[j]
         i = v.s
+        # remove all associated nodes
         for _ ∈ 1:v.n
             n = s.N[i]
             t = s.N[n.t]
@@ -88,6 +108,7 @@ function relatedvehicle!(rng::AbstractRNG, k::Int, s::Solution)
             removenode!(n, t, h, v, s)
             m += 1
         end
+        # update vehicle weight
         W[j] = 0.
         if m ≥ k break end
     end
@@ -95,12 +116,16 @@ function relatedvehicle!(rng::AbstractRNG, k::Int, s::Solution)
 end
 
 function worstvehicle!(rng::AbstractRNG, k::Int, s::Solution)
+    # set vehicle weights: utilization
     W = [v.l / v.q for v ∈ s.V]
+    # loop: until at least k nodes are removed
     m = 0
     while true
+        # sample a vehicle
         j = rand(rng, eachindex(s.V), Weights(W))
         v = s.V[j]
         i = v.s
+        # remove all associated nodes
         for _ ∈ 1:v.n
             n = s.N[i]
             t = s.N[n.t]
@@ -109,6 +134,7 @@ function worstvehicle!(rng::AbstractRNG, k::Int, s::Solution)
             removenode!(n, t, h, v, s)
             m += 1
         end
+        # update vehicle weight
         W[j] = 0.
         if m ≥ k break end
     end
