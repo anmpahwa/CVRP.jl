6167f8d9acf4d2d9b93c46a858a20b1c9d05d4de
anmpahwa <anmpahwa@civil.iitm.ac.in>
2025-09-12 12:38:11 +0530

    fix visualize

--- src/visualize.jl
+++ src/visualize.jl
@@ -23,13 +23,14 @@ function visualize(s::Solution; backend=gr)
     backend()
     fig = plot(legend=:none)
     # nodes
-    K = lastindex(s.N)
+    N = s.N
+    K = lastindex(N)
     X = zeros(Float64, K)       # abcissa
     Y = zeros(Float64, K)       # ordinate
     C = fill("color", K)        # color
     S = zeros(Int, K)           # size
     M = fill(:shape, K)         # marker
-    for (k,n) ∈ enumerate(s.N)
+    for (k,n) ∈ enumerate(N)
         X[k] = n.x
         Y[k] = n.y
         C[k] = isdepot(n) ? "#b4464b" : "#4682b4"
@@ -40,8 +41,8 @@ function visualize(s::Solution; backend=gr)
     # arcs
     Z = vectorize(s)
     K = lastindex(Z)
-    X = [Z[k].x for k ∈ 1:K]    # abcissa    
-    Y = [Z[k].y for k ∈ 1:K]    # ordinate
+    X = [N[Z[k]].x for k ∈ 1:K]    # abcissa    
+    Y = [N[Z[k]].y for k ∈ 1:K]    # ordinate
     plot!(X, Y, color="#23415a")
     # figure
     return fig
