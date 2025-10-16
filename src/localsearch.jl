bd7aae558d9d945a6fe54f2df92b599449d236dd
anmpahwa <anmpahwa@civil.iitm.ac.in>
2025-10-16 17:04:06 +0530

    update localsearch.jl

--- src/localsearch.jl
+++ src/localsearch.jl
@@ -65,17 +65,6 @@ function intermove!(rng::AbstractRNG, k::Int, s::Solution)
     V = G.V
     # initialize
     W = [isdepot(n) ? 0 : 1 for n ∈ N]
-    R = [zeros(Float64, eachindex(V)) for _ ∈ eachindex(V)]
-    for v ∈ V
-        for u ∈ V
-            if isequal(u,v) continue end
-            x = abs(u.x - v.x)
-            y = abs(u.y - v.y)
-            d = x + y
-            r = 1 / (d + 1e-3)
-            R[u.i][v.i] = r
-        end
-    end
     # iterate
     for _ ∈ 1:k
         # compute cost of the current solution
@@ -90,7 +79,7 @@ function intermove!(rng::AbstractRNG, k::Int, s::Solution)
         # sample a random vehicle and iterate through all positions in it
         c = 0.
         p = (t.i, h.i, v.i)
-        v = sample(rng, V, Weights(R[v.i]))
+        v = sample(rng, V)
         t = N[1]
         h = N[v.s]
         for _ ∈ 0:v.n
@@ -128,32 +117,18 @@ function intraswap!(rng::AbstractRNG, k::Int, s::Solution)
     # pre-initialize
     G = s.G
     N = G.N
-    A = G.A
     V = G.V
     # initialize
     W = [isdepot(n) ? 0 : 1 for n ∈ N]
-    R = [zeros(Float64, eachindex(N)) for _ ∈ eachindex(N)]
-    for n ∈ N
-        for m ∈ N
-            if isequal(n, m) continue end
-            if !isequal(n.v, m.v) continue end
-            d = A[n.i,m.i].c        
-            w = W[n.i] * W[m.i]
-            r = w / (d + 1e-3)
-            R[m.i][n.i] = r
-        end
-    end
     # iterate
     for _ ∈ 1:k
         # compute cost of the current solution
         z = f(s)
-        # sample a random node
-        n = sample(rng, N, Weights(W))
-        # sample another node from the same route, weighted by relatedness
-        m = sample(rng, N, Weights(R[n.i]))
+        # sample nodes
+        n, m = sample(rng, N, Weights(W), 2, replace=false)
+        # skip invalid configurations
+        if !isequal(n.v, m.v) continue end
         # swap the two nodes
-        if isdepot(m) continue end
-        if isequal(n,m) continue end
         tₙ = N[n.t]
         hₙ = N[n.h]
         vₙ = V[n.v]
@@ -222,28 +197,15 @@ function interswap!(rng::AbstractRNG, k::Int, s::Solution)
     V = G.V
     # initialize
     W = [isdepot(n) ? 0 : 1 for n ∈ N]
-    R = [zeros(Float64, eachindex(N)) for _ ∈ eachindex(N)]
-    for n ∈ N
-        for m ∈ N
-            if isequal(n, m) continue end
-            if isequal(n.v, m.v) continue end
-            d = A[n.i,m.i].c        
-            w = W[n.i] * W[m.i]
-            r = w / (d + 1e-3)
-            R[m.i][n.i] = r
-        end
-    end
     # iterate
     for _ ∈ 1:k
         # compute cost of the current solution
         z = f(s)
-        # sample a random node
-        n = sample(rng, N, Weights(W))
-        # sample another node from the same route, weighted by relatedness
-        m = sample(rng, N, Weights(R[n.i]))
+        # sample nodes
+        n, m = sample(rng, N, Weights(W), 2, replace=false)
+        # skip invalid configurations
+        if isequal(n.v, m.v) continue end
         # swap the two nodes
-        if isdepot(m) continue end
-        if isequal(n,m) continue end
         tₙ = N[n.t]
         hₙ = N[n.h]
         vₙ = V[n.v]
@@ -290,149 +252,6 @@ function interswap!(rng::AbstractRNG, k::Int, s::Solution)
             removenode!(m, tₘ, hₘ, vₘ, s)
             insertnode!(n, tₘ, hₘ, vₘ, s)
             insertnode!(m, tₙ, hₙ, vₙ, s)
-        end
-    end
-    # return solution
-    return s
-end
-
-function intraopt!(rng::AbstractRNG, k::Int, s::Solution)
-    # initialize
-    G = s.G
-    N = G.N
-    V = G.V
-    W = [isdepot(n) ? 0 : 1 for n ∈ N]
-    R = [[(isequal(n, m) || !isequal(n.v, m.v)) ? 0. : relatedness(n, m) for m ∈ N] for n ∈ N]
-    # iterate
-    for _ ∈ 1:k
-        # compute cost of the current solution
-        z  = f(s)
-        # sample a random node
-        n  = sample(rng, N, Weights(W))
-        vₙ = V[n.v]
-        # sample another node from the same route, weighted by relatedness
-        m  = sample(rng, N, Weights(R[n.i]))
-        vₘ = V[m.v]
-        # perform operations
-        if isdepot(m) continue end
-        if isequal(n,m) continue end
-        tₙ = N[n.i]
-        hₙ = N[n.h]
-        tₘ = N[m.t]
-        hₘ = N[m.h]
-        while !isequal(m, hₒ)
-            removenode!(m, tₘ, hₘ, vₘ, s)
-            insertnode!(m, tₙ, hₙ, vₙ, s)
-            tₙ = m 
-            hₙ = N[tₙ.h]
-            m  = tₘ
-            tₘ = N[m.t]
-            hₘ = N[m.h]
-        end
-        # evaluate the change in objective function value
-        z′ = f(s)
-        c  = z′ - z
-        if c < 0 continue end
-        # reperform operations
-        tₙ = N[n.i]
-        hₙ = N[n.h]
-        tₘ = N[m.t]
-        hₘ = N[m.h]
-        while !isequal(m, hₒ)
-            removenode!(m, tₘ, hₘ, vₘ, s)
-            insertnode!(m, tₙ, hₙ, vₙ, s)
-            tₙ = m 
-            hₙ = N[tₙ.h]
-            m  = tₘ
-            tₘ = N[m.t]
-            hₘ = N[m.h]
-        end
-    end
-    # return solution
-    return s
-end
-
-function interopt!(rng::AbstractRNG, k::Int, s::Solution)
-    # initialize
-    G = s.G
-    N = G.N
-    V = G.V
-    W = [isdepot(n) ? 0 : 1 for n ∈ N]
-    R = [[(isequal(n, m) || isequal(n.v, m.v)) ? 0. : relatedness(n, m) for m ∈ N] for n ∈ N]
-    # iterate
-    for _ ∈ 1:k
-        # compute cost of the current solution
-        z  = f(s)
-        # sample a random node
-        n  = sample(rng, N, Weights(W))
-        vₙ = V[n.v]
-        # sample another node from the same route, weighted by relatedness
-        m  = sample(rng, N, Weights(R[n.i]))
-        vₘ = V[m.v]
-        # perform operationss
-        if isdepot(m) continue end
-        if isequal(n,m) continue end
-        tₙ = N[n.i]
-        hₙ = N[n.h]
-        o  = N[m.i]
-        tₘ = N[m.t]
-        hₘ = N[m.h]
-        while !isdepot(o)
-            removenode!(o, tₘ, hₘ, vₘ, s)
-            insertnode!(o, tₙ, hₙ, vₙ, s)
-            tₙ = o 
-            hₙ = N[tₙ.h]
-            o  = hₘ
-            tₘ = N[o.t]
-            hₘ = N[o.h]
-        end
-        m  = hₙ
-        tₘ = N[vₘ.e]
-        hₘ = N[1]
-        o  = hₙ
-        tₙ = N[o.t]
-        hₙ = N[o.h]
-        while !isdepot(o)
-            removenode!(o, tₙ, hₙ, vₙ, s)
-            insertnode!(o, tₘ, hₘ, vₘ, s)
-            tₘ = o
-            hₘ = N[tₘ.h]
-            o  = hₙ
-            tₙ = N[o.t]
-            hₙ = N[o.h]
-        end
-        # evaluate the change in objective function value
-        z′ = f(s)
-        c  = z′ - z
-        if c < 0 continue end
-        # reperform operations
-        tₙ = N[n.i]
-        hₙ = N[n.h]
-        o  = N[m.i]
-        tₘ = N[m.t]
-        hₘ = N[m.h]
-        while !isdepot(o)
-            removenode!(o, tₘ, hₘ, vₘ, s)
-            insertnode!(o, tₙ, hₙ, vₙ, s)
-            tₙ = o 
-            hₙ = N[tₙ.h]
-            o  = hₘ
-            tₘ = N[o.t]
-            hₘ = N[o.h]
-        end
-        tₘ = N[vₘ.e]
-        hₘ = N[1]
-        o  = hₙ
-        tₙ = N[o.t]
-        hₙ = N[o.h]
-        while !isdepot(o)
-            removenode!(o, tₙ, hₙ, vₙ, s)
-            insertnode!(o, tₘ, hₘ, vₘ, s)
-            tₘ = o
-            hₘ = N[tₘ.h]
-            o  = hₙ
-            tₙ = N[o.t]
-            hₙ = N[o.h]
         end
     end
     # return solution
