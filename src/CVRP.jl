d618abcd5679fdfb17bd2d9402e70d041624dea6
anmpahwa <anmpahwa@civil.iitm.ac.in>
2025-10-17 07:53:04 +0530

    update CVRP

--- src/CVRP.jl
+++ src/CVRP.jl
@@ -6,6 +6,7 @@ using Random
 using Revise
 using StatsBase
 using DataFrames
+using ProgressMeter
 using Distributions
 
 include("datastructure.jl")
@@ -19,8 +20,19 @@ include("parameters.jl")
 include("ALNS.jl")
 include("visualize.jl")
 
-export  build, initialize,
-        vectorize, isfeasible, f, 
+export  Node, Arc, Vehicle, Graph, Solution,
+        build, initialize,
+        randomnode!, randomarc!, randomvehicle!, 
+        relatednode!, relatedarc!, relatedvehicle!, 
+        worstnode!, worstarc!, worstvehicle!,
+        bestprecise!, bestperturb!,
+        greedyprecise!, greedyperturb!,
+        regret2precise!, regret2perturb!,
+        regret3precise!, regret3perturb!,
+        intramove!, intermove!,
+        intraswap!, interswap!,
+        intraopt!, interopt!,
+        vectorize, isfeasible, f, h,
         ALNSparameters, ALNS, 
         visualize, pltcnv
 
