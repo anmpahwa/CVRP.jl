c029c2209fb4ecec4862d7cbc41c223574fe83b4
anmpahwa <anmpahwa@civil.iitm.ac.in>
2025-10-31 16:55:06 +0530

    update benchmark.jl

--- src/benchmark.jl
+++ src/benchmark.jl
@@ -1,4 +1,4 @@
-using VRP
+using CVRP
 using Revise
 using Random
 using CPUTime
@@ -6,45 +6,73 @@ using DataFrames
 
 let
     # Define instances
-    instances = []
+    instances = ["X-n101-k25"]
     # Define random number generators
     seeds = [1010, 1106, 1509, 1604, 1905, 2104, 2412, 2703, 2710, 2807]
     # Dataframes to store solution quality and run time
-    df₁ = DataFrame([instances, [zeros(length(instances)) for _ ∈ seeds]...], [iszero(i) ? "instance" : "$(seeds[i])" for i ∈ eachindex(seeds)])
-    df₂ = DataFrame([instances, [zeros(length(instances)) for _ ∈ seeds]...], [iszero(i) ? "instance" : "$(seeds[i])" for i ∈ eachindex(seeds)])
-    df₃ = DataFrame([instances, [zeros(length(instances)) for _ ∈ seeds]...], [iszero(i) ? "instance" : "$(seeds[i])" for i ∈ eachindex(seeds)])
-    df₄ = DataFrame([instances, [zeros(length(instances)) for _ ∈ seeds]...], [iszero(i) ? "instance" : "$(seeds[i])" for i ∈ eachindex(seeds)])
-    for i ∈ eachindex(instances)
-        instance = instances[i]
+    dfᶠ = DataFrame([instances, zeros(length(instances)), [zeros(length(instances)) for _ ∈ seeds]...], ["instance", "initial", ["$seed" for seed ∈ seeds]...])
+    dfᵗ = DataFrame([instances, zeros(length(instances)), [zeros(length(instances)) for _ ∈ seeds]...], ["instance", "initial", ["$seed" for seed ∈ seeds]...])
+    for (i,instance) ∈ enumerate(instances)
         # Visualize instance
-        display(visualize(instance))
-        for j ∈ eachindex(seeds)
-            seed = seeds[j]
-            println("\n instance: $instance | seed: $seed")
+        display(visualize("benchmark/$instance"))
+        # Define inital solution method and build the initial solution
+        t₁ = @elapsed s₁ = initialize("benchmark/$instance"; method=:dynamic);
+        # Visualize initial solution
+        display(visualize(s₁))
+        # Fetch solution characteristics
+        println("\nINSTANCE: $instance")
+        println("Initial Solution:")
+        println("   Run Time: $(round(t₁, digits=3)) seconds")
+        println("   Feasiblity: $(isfeasible(s₁)) | $(round(s₁.p, digits=3))")
+        println("   Objective Function: $(round(f(s₁), digits=3))")
+        # Store results
+        dfᶠ[i,2] = f(s₁)
+        dfᵗ[i,2] = t₁
+        # Save results
+        CSV.write("objective_function.csv", dfᶠ)
+        CSV.write("run_time.csv", dfᵗ)
+        for (j,seed) ∈ enumerate(seeds)
+            println("\nOptimal Solution | seed: $seed")
             rng = MersenneTwister(seed);
-            # Define inital solution method and build the initial solution
-            s₁ = initialize(rng, instance)
-            # Visualize initial solution
-            display(visualize(s₁))
             # Define ALNS parameters
-            x = max(100, lastindex(s₁.C))
+            x = max(100, lastindex(s₁.G.N))
             χ = ALNSparameters(
                 j   =   50                      ,
                 k   =   5                       ,
                 n   =   x                       ,
                 m   =   100x                    ,
-                Ψᵣ  =   [
-
-                        ]                       ,
-                Ψᵢ  =   [
-
-                        ]                       ,
-                Ψₗ  =   [
-
-                        ]                       ,
-                σ₁  =   15                      ,
-                σ₂  =   10                      ,
-                σ₃  =   3                       ,
+                Ψᵣ  =   (
+                            randomnode!         ,
+                            randomarc!          ,
+                            randomvehicle!      ,
+                            relatednode!        ,
+                            relatedarc!         ,
+                            relatedvehicle!     ,
+                            worstnode!          ,
+                            worstarc!           ,
+                            worstvehicle!
+                        )                       ,
+                Ψᵢ  =   (
+                            bestprecise!        ,
+                            bestperturb!        ,
+                            greedyprecise!      ,
+                            greedyperturb!      ,
+                            regret2precise!     ,
+                            regret2perturb!     ,
+                            regret3precise!     ,
+                            regret3perturb!
+                        )                       ,
+                Ψₗ  =   (
+                            intramove!          ,
+                            intermove!          ,
+                            intraswap!          ,
+                            interswap!          ,
+                            intraopt!           ,
+                            interopt!
+                        )                       ,
+                σ₁  =   15.0                    ,
+                σ₂  =   10.0                    ,
+                σ₃  =   3.0                     ,
                 μ̲   =   0.05                    ,
                 e̲   =   2                       ,
                 μ̅   =   0.2                     ,
@@ -57,39 +85,22 @@ let
                 ρ   =   0.1
             );
             # Run ALNS and fetch best solution
-            t = @CPUelapsed s₂ = ALNS(rng, χ, s₁);
+            t₂ = @elapsed s₂ = ALNS(rng, χ, s₁);
             # Visualize best solution
             display(visualize(s₂))
-            # Optimal solution characteristics
-            println("Optimal solution characteristics:")
-            nᵈ₁, nᵛ₁ = 0, 0
-            nᵈ₂, nᵛ₂ = 0, 0
-            for d ∈ s₁.D nᵈ₁ += VRP.isopt(d) end
-            for d ∈ s₂.D nᵈ₂ += VRP.isopt(d) end
-            for d ∈ s₁.D for v ∈ d.V nᵛ₁ += VRP.isopt(v) end end
-            for d ∈ s₂.D for v ∈ d.V nᵛ₂ += VRP.isopt(v) end end
-            # Fetch objective function values
-            println("Objective function value:")
-            println("   Initial: $(round(s₁.πᶠ + s₁.πᵒ, digits=3))")
-            println("   Optimal: $(round(s₂.πᶠ + s₂.πᵒ, digits=3))")
-            # Fetch lexicograohic objective function values
-            println("Lexicographic objective function value:")
-            println("   Initial: $(nᵛ₁) | $(round(s₁.πᵒ, digits=3))")
-            println("   Optimal: $(nᵛ₂) | $(round(s₂.πᵒ, digits=3))")
-            # Check if the solutions are feasible
-            println("Solution feasibility:")
-            println("   Initial: $(isfeasible(s₁)) | $(round(s₁.πᵖ, digits=3))")
-            println("   Optimal: $(isfeasible(s₂)) | $(round(s₂.πᵖ, digits=3))")
-            # Store Results
-            df₁[i,j+1] = s₂.πᶠ + s₂.πᵒ + s₂.πᵖ
-            df₂[i,j+1] = s₂.πᵒ + s₂.πᵖ
-            df₃[i,j+1] = nᵛ₂
-            df₄[i,j+1] = t
-            println(df₁)
-            println(df₂)
-            println(df₃)
-            println(df₄)
+            # Fetch solution characteristics
+            println("   Run Time: $(round(t₂, digits=3)) seconds")
+            println("   Feasiblity: $(isfeasible(s₂)) | $(round(s₂.p, digits=3))")
+            println("   Objective Function: $(round(f(s₂), digits=3))")
+            # Store results
+            dfᶠ[i,j+2] = f(s₂)
+            dfᵗ[i,j+2] = t₂
+            # Save results
+            CSV.write("objective_function.csv", dfᶠ)
+            CSV.write("run_time.csv", dfᵗ)
         end
+        println(dfᶠ)
+        println(dfᵗ)
     end
     return
 end 
